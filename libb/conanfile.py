ac1ef7e178ce828e987e78ebd3d839927e2c13c1
Axel Andersson <axel.andersson@volvocars.com>
2024-11-12 08:14:45 +0100

    Added libb

---
+++ libb/conanfile.py
@@ -0,0 +1,42 @@
+from io import StringIO
+
+from conan import ConanFile
+from conan.tools.cmake import CMake, CMakeDeps, CMakeToolchain, cmake_layout
+
+
+class libbRecipe(ConanFile):
+    name = "libb"
+    package_type = "static-library"
+
+    # Binary configuration
+    settings = "os", "compiler", "build_type", "arch"
+
+    # Sources are located in the same place as this recipe, copy them to the recipe
+    exports_sources = "CMakeLists.txt", "src/*", "include/*"
+
+    def set_version(self):
+        stdout = StringIO()
+        self.run("dunamai from git --style semver --bump", cwd=self.recipe_folder, stdout=stdout)
+        self.version = stdout.getvalue().strip()
+
+    def layout(self):
+        cmake_layout(self)
+
+    def generate(self):
+        deps = CMakeDeps(self)
+        deps.generate()
+        tc = CMakeToolchain(self)
+        tc.variables["LIBB_VERSION"] = self.version
+        tc.generate()
+
+    def build(self):
+        cmake = CMake(self)
+        cmake.configure()
+        cmake.build()
+
+    def package(self):
+        cmake = CMake(self)
+        cmake.install()
+
+    def package_info(self):
+        self.cpp_info.libs = ["libb"]
